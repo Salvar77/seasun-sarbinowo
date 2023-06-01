c8c43c8c1704c302d558fe606391e4fd4ffc0461
Kacper <mtscube@gmail.com>
2023-06-01 19:50:44 +0200

    dsas

--- pages/_app.js
+++ pages/_app.js
@@ -20,8 +20,9 @@ const MyApp = ({ Component, pageProps }) => {
 			<Script
 				async
 				src='https://www.googletagmanager.com/gtag/js?id=G-64FJ5CKMYE'
-			></Script>
-			<Script>
+				strategy='afterInteractive'
+			/>
+			<Script id='google-analytics' strategy='afterInteractive'>
 				window.dataLayer = window.dataLayer || []; function gtag()
 				{dataLayer.push(arguments)}
 				gtag('js', new Date()); gtag('config', 'G-64FJ5CKMYE');
