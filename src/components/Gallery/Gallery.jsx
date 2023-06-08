2e464570b5c4cedf6a9bee3c095195066852749d
Kacper <mtscube@gmail.com>
2023-06-08 12:45:25 +0200

    changes images

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -22,10 +22,29 @@ import Img17 from '@/assets/images/Sea_Sun_www-17.jpg';
 import Img18 from '@/assets/images/Sea_Sun_www-18.jpg';
 import Img19 from '@/assets/images/Sea_Sun_www-19.jpg';
 import Img20 from '@/assets/images/Sea_Sun_www-20.jpg';
-import kule from '@/assets/images/kule.jpg';
-import trawa from '@/assets/images/trawa.jpg';
-import trawa2 from '@/assets/images/trawa2.jpg';
-import laki from '@/assets/images/laki.jpg';
+import Oimg1 from '@/assets/images/Sea_Sun_outside_www-1.jpg';
+import Oimg2 from '@/assets/images/Sea_Sun_outside_www-2.jpg';
+import Oimg3 from '@/assets/images/Sea_Sun_outside_www-3.jpg';
+import Oimg4 from '@/assets/images/Sea_Sun_outside_www-4.jpg';
+import Oimg5 from '@/assets/images/Sea_Sun_outside_www-5.jpg';
+import Oimg6 from '@/assets/images/Sea_Sun_outside_www-6.jpg';
+import Oimg7 from '@/assets/images/Sea_Sun_outside_www-7.jpg';
+import Oimg8 from '@/assets/images/Sea_Sun_outside_www-8.jpg';
+import Oimg9 from '@/assets/images/Sea_Sun_outside_www-9.jpg';
+import Oimg10 from '@/assets/images/Sea_Sun_outside_www-10.jpg';
+import Oimg11 from '@/assets/images/Sea_Sun_outside_www-11.jpg';
+import Oimg12 from '@/assets/images/Sea_Sun_outside_www-12.jpg';
+import Oimg13 from '@/assets/images/Sea_Sun_outside_www-13.jpg';
+import Oimg14 from '@/assets/images/Sea_Sun_outside_www-14.jpg';
+import Oimg15 from '@/assets/images/Sea_Sun_outside_www-15.jpg';
+import Oimg16 from '@/assets/images/Sea_Sun_outside_www-16.jpg';
+import Oimg17 from '@/assets/images/Sea_Sun_outside_www-17.jpg';
+import Oimg18 from '@/assets/images/Sea_Sun_outside_www-18.jpg';
+import Oimg19 from '@/assets/images/Sea_Sun_outside_www-19.jpg';
+import Oimg20 from '@/assets/images/Sea_Sun_outside_www-20.jpg';
+import Oimg21 from '@/assets/images/Sea_Sun_outside_www-21.jpg';
+import Oimg22 from '@/assets/images/Sea_Sun_outside_www-22.jpg';
+
 import galleryHero from '@/assets/images/galleryHero.png';
 import HeroImg from '../HeroImg/HeroImg';
 import { useTranslation } from 'next-i18next';
@@ -43,31 +62,49 @@ const Gallery = () => {
 		});
 	}
 	const images = [
-		{ number: 1, src: kule },
-		{ number: 2, src: trawa },
-		{ number: 3, src: trawa2 },
-		{ number: 4, src: laki },
-		{ number: 5, src: Img1 },
-		{ number: 6, src: Img2 },
-		{ number: 7, src: Img3 },
-		{ number: 8, src: Img4 },
-		{ number: 9, src: Img5 },
-		{ number: 10, src: Img6 },
-		{ number: 11, src: Img7 },
-		{ number: 12, src: Img8 },
-		{ number: 13, src: Img9 },
-		{ number: 14, src: Img10 },
-		{ number: 15, src: Img11 },
-		{ number: 16, src: Img12 },
-		{ number: 17, src: Img13 },
-		{ number: 18, src: Img14 },
-		{ number: 19, src: Img15 },
-		{ number: 20, src: Img16 },
-		{ number: 21, src: Img17 },
-		{ number: 22, src: Img18 },
-		{ number: 23, src: Img19 },
-		{ number: 24, src: Img20 },
-	];
+		{ number: 1, src: Oimg8 },
+		{ number: 2, src: Oimg9 },
+		{ number: 3, src: Oimg10 },
+		{ number: 4, src: Oimg13 },
+		{ number: 5, src: Oimg5 },
+		{ number: 6, src: Oimg4 },
+		{ number: 7, src: Oimg21 },
+		{ number: 8, src: Oimg20 },
+		{ number: 9, src: Oimg1 },
+		{ number: 10, src: Img1 },
+		{ number: 11, src: Img2 },
+		{ number: 12, src: Img3 },
+		{ number: 13, src: Img4 },
+		{ number: 14, src: Img5 },
+		{ number: 15, src: Img6 },
+		{ number: 16, src: Img7 },
+		{ number: 17, src: Img8 },
+		{ number: 18, src: Img9 },
+		{ number: 19, src: Img10 },
+		{ number: 20, src: Img11 },
+		{ number: 21, src: Img12 },
+		{ number: 22, src: Img13 },
+		{ number: 23, src: Img14 },
+		{ number: 24, src: Oimg14 },
+		{ number: 25, src: Img15 },
+		{ number: 26, src: Img16 },
+		{ number: 27, src: Oimg16 },
+		{ number: 28, src: Img17 },
+		{ number: 29, src: Img18 },
+		{ number: 30, src: Img19 },
+		{ number: 31, src: Img20 },
+		{ number: 32, src: Oimg12 },
+		{ number: 33, src: Oimg3 },
+		{ number: 34, src: Oimg15 },
+		{ number: 35, src: Oimg11 },
+		{ number: 36, src: Oimg22 },
+		{ number: 37, src: Oimg2 },
+		{ number: 38, src: Oimg17 },
+		{ number: 39, src: Oimg7 },
+		{ number: 40, src: Oimg6 },
+		{ number: 41, src: Oimg18 },
+		{ number: 42, src: Oimg19 },
+	  ];
 
 	return (
 		<>
